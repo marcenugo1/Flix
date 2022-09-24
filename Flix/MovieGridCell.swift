fd6f96cbb35272f60df9b81e0e714d6781182cdb
marcenugo1 <marce.nugo1@gmail.com>
2022-09-23 21:49:48 -0500

    Assignment 2
    Code for the assigment 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by marcenugo on 23/09/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
